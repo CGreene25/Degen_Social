b5465cbd7c5d6a3930e54e707a895022aa6fcf47
CGreene25 <cgreene4@trincoll.edu>
2022-04-02 12:09:18 -0400

    start

--- contracts/DegenSocial.sol
+++ contracts/DegenSocial.sol
@@ -5,24 +5,47 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DegenSocial is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract MYToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        _mint(msg.sender, initialSupply);
+    }
+}
 
-    constructor() ERC721("MyNFT", "NFT") {}
+contract DegenSocial {
+    string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    struct Post {
+        uint256 postCount;
+        string name;
+        address owner;
+        //string content;
+    }
 
-        return newItemId;
+    event PostCreated(
+        uint256 postCount,
+        string name,
+        address owner//,
+    //string content
+    );
+    constructor() public {
+        name = "Social APP";
+    }
+
+    function createPost(string memory _name, string memory _content) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        //Require valid content
+        require(bytes(_content).length > 0);
+        // Increment product count
+        postCount ++;
+        // Create the product
+        posts[postCount] = Post(postCount, _name, msg.sender);
+        // Trigger an event
+        emit PostCreated(postCount, _name, msg.sender);
     }
 }
