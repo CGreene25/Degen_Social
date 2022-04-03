f1bf9f048b0e5802421ad4b38fd4c4297180e9b9
CGreene25 <cgreene4@trincoll.edu>
2022-04-03 13:42:53 -0400

    update to rewards

--- contracts/DegenSocial.sol
+++ contracts/DegenSocial.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DegenSocial is ERC20 {
     uint public postCount = 0;
-    uint public likeCount = 0;
     mapping(uint => Post) public posts;
 
     struct Post {
@@ -25,12 +24,44 @@ contract DegenSocial is ERC20 {
     event PostLiked(address owner, uint256 likeCount);
 
     constructor() ERC20("DegenCoin", "DGN") {
+
+        //mint the initial supply
         _mint(msg.sender, 10000000);
     }
 
-    function mintMinerReward() public {
-        _mint(block.coinbase, 10);
+    function createPost(string memory _name, string memory _content) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        //Require valid content
+        require(bytes(_content).length > 0);
+        // Increment product count
+        postCount ++;
+        uint256 likeCount = 0;
+        posts[postCount] = (Post(_name, msg.sender, _content, postCount, likeCount, new address[] (0)));
+        // Trigger an event
+        emit PostCreated(_name, _content);
     }
+    function likePost(uint256 postCount, address _owner) public {
+        bool newLike = true;
+        // Require unused ID
+        for(uint i=0; i<posts[postCount].likes.length;i++){
+            if(_owner != posts[postCount].likes[i]){
+                newLike = false;
+            }
+        }
+        if(newLike == true){
+            //Increase like count
+            posts[postCount].likeCount ++;
+            //add address to array of likers
+            posts[postCount].likes.push(_owner);
+            //mint miner reward
+            _mint(posts[postCount].owner, 5);
+        }
+        // Trigger an event
+        emit PostLiked(_owner, likeCount);
+    }
+
+    //_____GETTER FUNCTIONS_____//
 
     function getPostContent() public view returns (string[] memory){
         string[] memory ret = new string[](postCount);
@@ -63,38 +94,5 @@ contract DegenSocial is ERC20 {
         }
         return ret;
     }
-
-    function createPost(string memory _name, string memory _content) public {
-        // Require a valid name
-        require(bytes(_name).length > 0);
-        //Require valid content
-        require(bytes(_content).length > 0);
-        // Increment product count
-        postCount ++;
-        uint256 likeCount = 0;
-        posts[postCount] = (Post(_name, msg.sender, _content, postCount, likeCount, new address[] (0)));
-        // Trigger an event
-        emit PostCreated(_name, _content);
-    }
-    function likePost(uint256 postCount, address _owner) public {
-        bool newLike = true;
-        // Require unused ID
-        for(uint i=0; i<posts[postCount].likes.length;i++){
-            if(_owner != posts[postCount].likes[i]){
-                newLike = false;
-            }
-        }
-        if(newLike == true){
-            //Increase like count
-            posts[postCount].likeCount ++;
-            //add address to array of likers
-            posts[postCount].likes.push(_owner);
-            //mint miner reward
-            mintMinerReward();
-        }
-        // Trigger an event
-        emit PostLiked(_owner, likeCount);
-    }
-
 
 }
